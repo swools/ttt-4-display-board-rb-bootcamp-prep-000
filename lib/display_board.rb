5607c3f8071b71f192a0a81c570d77ac2a46c227
Stephen Wool <stevewool7@gmail.com>
2019-04-18 15:11:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(arr)
   puts " #{arr[0]} | #{arr[1]} | #{arr[2]} "
   puts "-----------"
-  puts "#{arr[3]} | #{arr[4]} | #{arr[5]}"
+  puts " #{arr[3]} | #{arr[4]} | #{arr[5]} "
   puts "-----------"
-  puts "#{arr[6]} | #{arr[7]} | #{arr[8]}"
+  puts " #{arr[6]} | #{arr[7]} | #{arr[8]} "
 end
