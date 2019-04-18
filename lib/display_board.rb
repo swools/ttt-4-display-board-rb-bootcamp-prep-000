14516ec2c7400e7c107a6b6ed72b07c02b28612f
Stephen Wool <stevewool7@gmail.com>
2019-04-18 15:00:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,9 @@
 # out the current state.
 board = ["", "", "", "", "", "", "", "", ""]
 
-build_board(board)
+def display_board(board)
+print "#{board[0]} | #{board[1]} | #{board[2]}"
+print "-----------"
+print "#{board[3]} | #{board[4]} | #{board[5]}"
+print "-----------"
+print "#{board[6]} | #{board[7]} | #{board[8]}"
